a53392cd6ed99ede15ca8b2a5d7014a82bb099fa
lisprolog <lisprolog@users.noreply.github.com>
2017-07-11 16:29:09 +0100

    Delete count-instances.lisp

--- count-instances.lisp
+++
@@ -1,8 +0,0 @@
-(defun count-instances (obj lists)
-	(labels ((instances-in (lst)
-		(if 	(consp lst)
-			(+ 	(if (eq (car lst) obj) 1 0)
-				(instances-in (cdr lst)))
-		0)))
-	(mapcar #'instances-in lsts)))
-
